8389f286dc58f1f905d0acbae06c735689fb0642
Michael <github@mjpeter.de>
2026-03-22 14:20:07 +0100

    chore: Add EmojiConfig type annoation

--- lua/emoji/config.lua
+++ lua/emoji/config.lua
@@ -1,18 +1,27 @@
 local M = {}
 
-local defaults = { enable_cmp_integration = false, plugin_path = vim.fn.stdpath("data") .. "/lazy/" }
+---@class EmojiConfig
+---@field enable_cmp_integration boolean
+---@field plugin_path string
 
-M.options = {}
+---@class EmojiConfigPartial
+---@field enable_cmp_integration? boolean
+---@field plugin_path? string
+
+---@type EmojiConfig
+M.options = {
+  enable_cmp_integration = false,
+  plugin_path = vim.fn.stdpath("data") .. "/lazy/",
+}
 
 M.paths = {
   emoji = "emoji.nvim/lua/data/emojis.json",
   kaomoji = "emoji.nvim/lua/data/kaomojis.json",
 }
 
+---@param options EmojiConfigPartial?
 function M.setup(options)
-  M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
+  M.options = vim.tbl_deep_extend("force", {}, M.options, options or {})
 end
-
-M.setup()
 
 return M
